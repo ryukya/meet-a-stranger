2a5db7191ab56c55f8340bfe564601dcfc694013
ryukya <felisiaryukiya@gmail.com>
2022-06-17 16:42:37 +0200

    001-First trial commit

--- src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
+++ src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -1,6 +1,12 @@
 package com.epam.rd.autotasks.meetastranger;
+
+import java.util.Scanner;
+
 public class MeetAStranger {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, "+name);
         //Write a program, which read a String from System.in and print "Hello, <input string>"
     }
 }
